85e4b35ad156b220406a6975fff9fa44f3d68592
SUFP6\Terry Nirv <anti_meson@hotmail.com>
2021-08-01 17:02:36 -0700

    use parametrize

--- tests/test_pw_gen.py
+++ tests/test_pw_gen.py
@@ -9,34 +9,22 @@ def test_phrase():
         assert isinstance(result, str)
         assert len(result) >= min_len and len(result) <= max_len
 
-def test_pw_space():
-    samples = [
-        " ",
-        "                                ",
-    ]
-    for sample in samples:
-        for _ in range(1000):
-            result = pw_gen.pwgen(sample)
-            assert " " not in result
-            assert isinstance(result, str)
-            assert len(result) >= pw_gen.pw_config["length_min"] and len(result) <= pw_gen.pw_config["length_max"]
-
-def test_pw_directinput():
-    samples = [
-        "  12345abcde  ",
-        "  @#$1234abc  "
-    ]
-    for sample in samples:
+@pytest.mark.parametrize("sample",[" ", "       "])
+def test_pw_space(sample):
+    for _ in range(1000):
         result = pw_gen.pwgen(sample)
         assert " " not in result
         assert isinstance(result, str)
-        assert sample.replace(" ","") == result
+        assert len(result) >= pw_gen.pw_config["length_min"] and len(result) <= pw_gen.pw_config["length_max"]
 
-def test_pw_ditectinput_error():
-    samples = [
-        "12@",
-        " 123a@"
-    ]
+@pytest.mark.parametrize("sample",["  12345abcde  ", "  @#$1234abc  "])
+def test_pw_directinput(sample):
+    result = pw_gen.pwgen(sample)
+    assert " " not in result
+    assert isinstance(result, str)
+    assert sample.replace(" ","") == result
+
+@pytest.mark.parametrize("sample",["12@", " @#$abc123"])
+def test_pw_ditectinput_error(sample):
     with pytest.raises(ValueError):
-        for sample in samples:
-            pw_gen.pwgen(sample)
+        pw_gen.pwgen(sample)
